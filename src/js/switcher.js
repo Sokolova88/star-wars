c302181f0845dd14fd2c889664059df3ebacb14f
Sokolova88 <[sokolova.studies@gmail.com]>
2024-03-21 22:52:12 +0200

    Add @media for adaptive hero section

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,6 +1,7 @@
 const darkTheme = {
   accent: '#FFC700',
   mainText: '#fff',
+  heroText: '#fff',
   sectionBg: '#222527',
   overlay: 'rgb(239 236 236 / 90%)',
   mainBg: '#060803',
@@ -9,6 +10,7 @@ const darkTheme = {
 const lightTheme = {
   accent: '#4B526A',
   mainText: '#15141D',
+  heroText: '#7a7982',
   sectionBg: '#EBEBEB',
   overlay: 'rgb(21 20 29 / 90%)',
   mainBg: '#fff',
@@ -30,6 +32,7 @@ function switchTheme() {
     element.style.setProperty('--theme-sectionBg', lightTheme.sectionBg);
     element.style.setProperty('--theme-overlay', lightTheme.overlay);
     element.style.setProperty('--theme-mainBg', lightTheme.mainBg);
+    element.style.setProperty('--theme-heroText', lightTheme.heroText);
 
     heroContainer.classList.add('hero-section--light');
     codexContainer.classList.add('codex-section--light');
@@ -41,6 +44,7 @@ function switchTheme() {
     element.style.setProperty('--theme-sectionBg', darkTheme.sectionBg);
     element.style.setProperty('--theme-overlay', darkTheme.overlay);
     element.style.setProperty('--theme-mainBg', darkTheme.mainBg);
+    element.style.setProperty('--theme-heroText', darkTheme.heroText);
 
     heroContainer.classList.remove('hero-section--light');
     codexContainer.classList.remove('codex-section--light');
